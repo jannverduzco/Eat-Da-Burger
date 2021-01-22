f51e9c7cb6b86702eb44d4d88700de898a61c72b
Janneth Verduzco <janneth_verduzco@live.com>
2021-01-22 09:01:16 -0600

    added module.exports=router: to burgersConttollers.js

--- controllers/burgersControllers.js
+++ controllers/burgersControllers.js
@@ -4,3 +4,6 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var cat = require("../model/burger.js");
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+ id INT AUTO_INCREMENT
+)
