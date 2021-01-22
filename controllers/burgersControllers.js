f73d6a7cb33ba228f042a86c54732ab8c9b5b4a4
Janneth Verduzco <janneth_verduzco@live.com>
2021-01-22 09:32:25 -0600

    updated burgersController.js in controllers FOLDER

--- controllers/burgersControllers.js
+++ controllers/burgersControllers.js
@@ -19,9 +19,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
     burger.create([
-        "name", "sleepy"
+        "burger_name", "devoured"
     ], [
-        req.body.name, req.body.sleepy
+        req.body.burgerName, req.body.devoured
     ], function(result) {
         // Send back the ID of the new quote
         res.json({ id: result.insertId });
@@ -34,7 +34,7 @@ router.put("/api/burgers/:id", function(req, res) {
     console.log("condition", condition);
 
     burger.update({
-        sleepy: req.body.sleepy
+        devoured: req.body.devoured
     }, condition, function(result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
